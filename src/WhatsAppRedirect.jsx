a9f8d16af8eb24feeb35b820d43f6b3bcb262628
sahoochinmay <sahoochinmay54@gmail.com>
2024-01-04 19:21:52 +0530

    fd

--- src/WhatsAppRedirect.jsx
+++ src/WhatsAppRedirect.jsx
@@ -2,43 +2,8 @@ import React, { useEffect } from "react";
 
 const WhatsAppRedirect = () => {
   useEffect(() => {
-    // Check if WhatsApp is installed
-    // isAppInstalled("https://wa.me/918837849092?text=Kindly%20Click%20on%20Send%20Message%20Button%20qr%3Fi%3D");
-    // if (isWhatsAppInstalled) {
-    //   // Redirect to WhatsApp
-    //   window.location.href = "whatsapp://send?text=Hello";
-    // } else {
-    //   // Show alert and stay in your app
-    //   alert("WhatsApp is not installed on your device.");
-    // }
-    // handleWhatsappRedirect();
+    handleWhatsAppClick();
   }, []);
-
-  //   const isAppInstalled = (urlScheme) => {
-  //     const iframe = document.createElement("iframe");
-  //     // iframe.style.display = "none";
-  //     iframe.src = urlScheme;
-  //     document.body.appendChild(iframe);
-
-  //     // const timeout = setTimeout(() => {
-  //     //   document.body.removeChild(iframe);
-  //     // }, 1000);
-
-  //     // iframe.onload = () => {
-  //     //   clearTimeout(timeout);
-  //     //   document.body.removeChild(iframe);
-  //     // };
-
-  //     // return true; // Assuming WhatsApp is installed, change as needed
-  //   };
-  //   const handleWhatsappRedirect = async () => {
-  //     // Check for WhatsApp installation on desktop browsers
-  //     const phoneNumber = "918837849092";
-  //     const encodedText = encodeURIComponent("Kindly Click on Send Message Button qr?i=");
-  //     const whatsappLink = `https://api.whatsapp.com/send/?phone=${phoneNumber}&text=${encodedText}`;
-
-  //     window.location.href = whatsappLink;
-  //   };
   const handleWhatsAppClick = () => {
     const phoneNumber = "9937159477";
     const text = "hi";
@@ -65,15 +30,8 @@ const WhatsAppRedirect = () => {
   };
   return (
     <div>
-      {/* Your component content */}
       <p>WhatsApp Redirect Page</p>
-      {/* <iframe src="https://api.whatsapp.com/send/?phone=9937159477&text=hi"></iframe> */}
-      <a href="whatsapp://send/?phone=9937159477&text=hi" target="_blank" rel="noreferrer">
-        {" "}
-        click
-      </a>
-      <iframe src="whatsapp://send/?phone=9937159477&text=hi"></iframe>
-      <button onClick={handleWhatsAppClick}>handleWhatsAppClick</button>
+      <button>WhatsApp is not installed on your device. </button>
     </div>
   );
 };
