3858a92fb7873f43233c87f1d59f997568b40bab
sahoochinmay <sahoochinmay54@gmail.com>
2024-01-04 19:26:41 +0530

    new link

--- src/WhatsAppRedirect.jsx
+++ src/WhatsAppRedirect.jsx
@@ -12,21 +12,21 @@ const WhatsAppRedirect = () => {
     // Attempt to open the WhatsApp link
     window.location.href = whatsappLink;
 
-    // Set a timeout to check if the user navigated away
-    const timeout = setTimeout(() => {
-      // If the user hasn't navigated away, show an alert
-      if (document.hidden) {
-        alert("WhatsApp is not installed on your device.");
-      }
-    }, 2000); // Adjust the timeout as needed
+    // // Set a timeout to check if the user navigated away
+    // const timeout = setTimeout(() => {
+    //   // If the user hasn't navigated away, show an alert
+    //   if (document.hidden) {
+    //     alert("WhatsApp is not installed on your device.");
+    //   }
+    // }, 2000); // Adjust the timeout as needed
 
-    // Listen for visibility changes
-    document.addEventListener("visibilitychange", () => {
-      // If the user navigated away, clear the timeout
-      if (document.hidden) {
-        clearTimeout(timeout);
-      }
-    });
+    // // Listen for visibility changes
+    // document.addEventListener("visibilitychange", () => {
+    //   // If the user navigated away, clear the timeout
+    //   if (document.hidden) {
+    //     clearTimeout(timeout);
+    //   }
+    // });
   };
   return (
     <div>
